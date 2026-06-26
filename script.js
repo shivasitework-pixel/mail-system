0eefbb5bad125882598013ce39fde8b04e635f45
shivasitework-pixel <shivasitework@gmail.com>
2026-06-26 06:30:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function login(){
 
+let email=document.getElementById("email").value;
+
+let password=document.getElementById("password").value;
+
+if(email==="" || password===""){
+document.getElementById("msg").innerHTML="Please fill all fields";
+return;
+}
+
+document.getElementById("msg").innerHTML="Login Successful (Demo)";
+}
